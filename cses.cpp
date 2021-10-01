700f9e66db737099b5ffebf90b34c1a846206a1e
Chetan Kumar <chetan18026@gmail.com>
2021-10-02 00:28:28 +0530

    cses

---
+++ cses.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define frfr  freopen("input.txt","r",stdin); //freopen("output.txt","w",stdout);
+#define endl "\n"
+#define loop(i,n) for(int i=0;i<n;i++)
+#define ll long long int
+
+int main(){
+	int i = 10;
+	void *p = &i;
+	cout << (int)* p ;
+	return 0;
+	
+}
